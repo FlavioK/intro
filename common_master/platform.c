0bb52da346c39fda478da029934b47f4f2730e93
FlavioK <flavio.kreiliger@bluewin.ch>
2015-09-25 15:26:51 +0200

    added platfom files

---
+++ common_master/platform.c
@@ -0,0 +1,8 @@
+/*
+ * platform.c
+ *
+ *  Created on: 25.09.2015
+ *      Author: Flavio
+ */
+
+
---
+++ common_master/platform.h
@@ -0,0 +1,13 @@
+/*
+ * platform.h
+ *
+ *  Created on: 25.09.2015
+ *      Author: Flavio
+ */
+
+#ifndef SOURCES_COMMON_MASTER_PLATFORM_H_
+#define SOURCES_COMMON_MASTER_PLATFORM_H_
+
+
+
+#endif /* SOURCES_COMMON_MASTER_PLATFORM_H_ */
