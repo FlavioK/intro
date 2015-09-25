04f0949e0691791f14bf888c44e3603be99954b7
FlavioK <flavio.kreiliger@bluewin.ch>
2015-09-25 15:27:16 +0200

    added platform_local.h

---
+++ robo/Sources/platform_local.h
@@ -0,0 +1,13 @@
+/*
+ * platform_local.h
+ *
+ *  Created on: 25.09.2015
+ *      Author: Flavio
+ */
+
+#ifndef SOURCES_PLATFORM_LOCAL_H_
+#define SOURCES_PLATFORM_LOCAL_H_
+
+
+
+#endif /* SOURCES_PLATFORM_LOCAL_H_ */
